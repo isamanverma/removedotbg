fa4c5df5e039168b76e0463c841db60041359345
amankumarverma <theamankumarverma@gmail.com>
2025-02-26 03:02:13 +0530

    webhooks added

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Connect to MongoDB
 connectDB();
@@ -20,8 +23,10 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
+app.use("/api/user", userRouter);
+
 // For local development
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log(`✅ App is running on:\nhttp://localhost:${PORT}/`);
